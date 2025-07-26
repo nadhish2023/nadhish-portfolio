1e897fdaa1f94f95ec9f23e6317768f66626de69
Nadhish SN <nadhish.sn@gmail.com>
2025-07-26 16:12:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/nadhish_portfolio"
+  base: process.env.VITE_BASE_PATH || "/FUTURE_FS_01"
 })
